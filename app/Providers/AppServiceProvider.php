7cea45897076b9d157744ff5c0efeca41a8c57fa
Birbsilog <princedequiros@gmail.com>
2026-01-21 23:02:09 +0800

    Fix AppServiceProvider and add missing RouteServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,45 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
 
-class RouteServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
     /**
-     * The path to the "home" route for your application.
-     *
-     * Users will be redirected here after login if no other redirect is set.
+     * Register any application services.
      */
-    public const HOME = '/'; // 👈 tourist homepage (landing page)
-
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     */
-    public function boot(): void
+    public function register(): void
     {
-        $this->configureRateLimiting();
-
-        $this->routes(function () {
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-
-            Route::prefix('api')
-                ->middleware('api')
-                ->group(base_path('routes/api.php'));
-        });
+        //
     }
 
     /**
-     * Configure the rate limiters for the application.
+     * Bootstrap any application services.
      */
-    protected function configureRateLimiting(): void
+    public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        //
     }
 }
